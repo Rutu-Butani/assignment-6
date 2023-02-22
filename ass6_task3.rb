a96e871368de3cbf74ff83d563ac872013226bc9
Rutu-Butani <125644767+Rutu-Butani@users.noreply.github.com>
2023-02-22 22:30:52 +0530

    Update ass6_task3.rb

--- ass6_task3.rb
+++ ass6_task3.rb
@@ -1,6 +1,9 @@
-elements = ['two', 'three', 'one']
-hash_to_be_sorted = {'three' => 3, 'two' => 2, 'one' => 1}
+hash = { "three" => 3, "two" => 2, "one" => 1 }
+array = ["two", "three", "one"]
 
-sorted_hash = hash_to_be_sorted.to_a.sort_by { |k, v| elements.index(k) }.to_h
-puts sorted_hash 
-
+new_hash = {}
+    array.each do |key, value|
+    new_hash[key] = hash[key]
+    
+end
+puts new_hash
